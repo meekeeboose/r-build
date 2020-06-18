2430b84cee1f3b576fd9f135f73759b99cdd55dd
meekeboose <meekeboose@gmail.com>
2020-06-18 23:59:31 +0800

    initial

---
+++ precache-manifest.c4a7934ae4589f89b383a1fe79236dfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01ad7b5a5a3bb69f737685f41691fbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fa5a7de8e8f5303d61d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4583ccbf68222b55506",
+    "url": "/static/js/2.930fdcc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.930fdcc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa5a7de8e8f5303d61d",
+    "url": "/static/js/main.f2e211fb.chunk.js"
+  },
+  {
+    "revision": "f5467cbe7218edf87022",
+    "url": "/static/js/runtime-main.9c5b1d32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
